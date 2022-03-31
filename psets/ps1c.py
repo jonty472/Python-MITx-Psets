94220495fb5e95fc442e613e2303c16659b2b2d7
JontyBurden <azttorrs@gmail.com>
2022-03-31 17:33:13 +1300

    replanning

--- psets/ps1c.py
+++ psets/ps1c.py
@@ -1,7 +1,7 @@
 annual_salary = 150000
 #float(input('What is your annual salary: '))
 
-portion_saved = 0
+#portion_saved = 0
 #float(input('Enter the percent of your salary to save, as a decimal: '))
 
 total_cost = 1000000
@@ -22,35 +22,65 @@ dp_monthly = downpayment/36
 
 monthly_salary = 12500
 
-saving_rate = dp_monthly/monthly_salary
-
-#print(saving_rate)
-
 
 month_count = 0
-
 
 """
 write a program to calculate the best savings rate, as a function of your starting salary.
 
 How much should you save each month to achieve this?
 
-apply annual_raise for the 36 months to begin with then divide 12 to get yearly annual salary for x
+Tip - You can change the value being assesed in a while loop from inside (e.g. our salary changes every 6 months
+so we need to take this into account)
+Plan -
 
-apply the banks_intrest in the inside the low and high if statements
+monthly_pay = annual_pay/12
+
+low = 0.0
+high = max(0.0, monthly pay)
+
+#remeber values only being directly assesd can be changed
+savings_rate = (high + low)/2.0
+
+while savings <= downpayment:
+    numGuess+=1
+    month_count+=1
+
+    if ans*36 < downpayment:
+        low = high
+    else:
+        high = ans
+
+    if month_count%6 == 0:
+        apply_raise
+        re-evulate monthly pay so ans takes in new variable
+    
+    current_saings = current_savings += (annual_salary/12)*(portion_saved)
 """
 
-x = annual_salary/12
-epsilon = 0.01
+x = monthly_salary
 numGuesses = 0
 low = 0.0
 high = max(1.0, x)
 ans = (high + low)/2.0
 
-while ans*36 != downpayment:
-    #print('low =', low, 'high =', high, 'ans =', ans)
+saving_rate = dp_monthly/monthly_salary
+
+# Calculates the monthly deposit need for 36 months
+while current_savings<=downpayment:
+    print('low =', low, 'high =', high, 'ans =', ans)
     numGuesses+=1
-    
+    month_count+=1
+
+    if month_count%6 == 0:
+        annual_salary += annual_salary*annual_raise
+        monthly_salary = annual_salary/12
+        ans = (max(1.0,(monthly_salary)) + low)/2.0
+        print('raise applied: ', annual_salary)
+
+    current_savings += (monthly_salary)*(saving_rate)
+    #current_savings += current_savings*r/12
+
     if ans*36 < downpayment:
         low = ans
     else:
@@ -60,5 +90,5 @@ while ans*36 != downpayment:
 
 print(ans/monthly_salary)
 print(numGuesses)
-
+print('current savings:',current_savings)
     
