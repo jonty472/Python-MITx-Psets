5b3b841abc6da30d12b002f9ee48dcba4b47755e
JontyBurden <azttorrs@gmail.com>
2022-04-02 13:27:45 +1300

    need to figure out how to incoperate the banks intrest of savings

--- psets/ps1c.py
+++ psets/ps1c.py
@@ -1,13 +1,10 @@
 annual_salary = 150000
 #float(input('What is your annual salary: '))
 
-#portion_saved = 0
-#float(input('Enter the percent of your salary to save, as a decimal: '))
-
 total_cost = 1000000
 #float(input('What is the current cost of your dream home: '))
 
-annual_raise = .07
+annual_raise = 0.07
 #float(input('Enter the semi­annual raise, as a decimal:'))
 
 portion_down_payment = 0.25
@@ -16,79 +13,58 @@ current_savings = 0
 
 r = 0.04 # ROI
 
-downpayment = 250000
-
-dp_monthly = downpayment/36
-
-monthly_salary = 12500
-
-
 month_count = 0
 
-"""
-write a program to calculate the best savings rate, as a function of your starting salary.
-
-How much should you save each month to achieve this?
-
-Tip - You can change the value being assesed in a while loop from inside (e.g. our salary changes every 6 months
-so we need to take this into account)
-Plan -
-
-monthly_pay = annual_pay/12
-
-low = 0.0
-high = max(0.0, monthly pay)
-
-#remeber values only being directly assesd can be changed
-savings_rate = (high + low)/2.0
-
-while savings <= downpayment:
-    numGuess+=1
-    month_count+=1
-
-    if ans*36 < downpayment:
-        low = high
-    else:
-        high = ans
-
-    if month_count%6 == 0:
-        apply_raise
-        re-evulate monthly pay so ans takes in new variable
-    
-    current_saings = current_savings += (annual_salary/12)*(portion_saved)
-"""
-
-x = monthly_salary
 numGuesses = 0
-low = 0.0
+
+"""
+ Python 3, if we want to use integer division instead of the standard float division, we can use the // operator:
+
+"""
+downpayment = total_cost*portion_down_payment
+
+x = 250000
+
 high = max(1.0, x)
-ans = (high + low)/2.0
 
-saving_rate = dp_monthly/monthly_salary
+low = 0.0
 
-# Calculates the monthly deposit need for 36 months
-while current_savings<=downpayment:
+ans = (high + low)//2
+
+#Accepts downpayment within + or - 100
+while ans*36<=(downpayment - 100) or ans*36>=(downpayment+100):
     print('low =', low, 'high =', high, 'ans =', ans)
     numGuesses+=1
-    month_count+=1
-
-    if month_count%6 == 0:
-        annual_salary += annual_salary*annual_raise
-        monthly_salary = annual_salary/12
-        ans = (max(1.0,(monthly_salary)) + low)/2.0
-        print('raise applied: ', annual_salary)
-
-    current_savings += (monthly_salary)*(saving_rate)
-    #current_savings += current_savings*r/12
 
     if ans*36 < downpayment:
         low = ans
     else:
         high = ans
-    ans = (high+low)/2.0
-
-
-print(ans/monthly_salary)
-print(numGuesses)
-print('current savings:',current_savings)
     
+    ans = (high + low)//2
+
+print(numGuesses)
+
+
+while month_count != 36 and current_savings <= downpayment:
+    month_count+=1
+
+    if month_count%6 ==0:
+        annual_salary += annual_salary*annual_raise
+        #print('annual raise applied:', annual_salary)
+
+    savings_rate = ans/(annual_salary/12)
+    #print("{:.4f}".format(savings_rate))
+
+    
+    current_savings += savings_rate*(annual_salary/12)
+    print('current savings:',current_savings,'at month count:',month_count)
+
+    # ROI
+    current_savings += current_savings*r/12
+    
+
+
+#print('current savings:',current_savings)
+#print('month count:',month_count)
+
