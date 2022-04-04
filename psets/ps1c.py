699c3f39880b0ea846e4f614eaa2c65bcf8b864f
JontyBurden <azttorrs@gmail.com>
2022-04-04 19:17:58 +1200

    done

--- psets/ps1c.py
+++ psets/ps1c.py
@@ -1,70 +1,64 @@
-annual_salary = 150000
-#float(input('What is your annual salary: '))
+annual_salary = float(input('What is your annual salary: '))
 
 total_cost = 1000000
-#float(input('What is the current cost of your dream home: '))
+
 
 annual_raise = 0.07
-#float(input('Enter the semi­annual raise, as a decimal:'))
+
 
 portion_down_payment = 0.25
 
 current_savings = 0
 
-r = 0.04 # ROI
+roi = 0.04 # Return on investment
 
-month_count = 0
-
-numGuesses = 0
-
-"""
- Python 3, if we want to use integer division instead of the standard float division, we can use the // operator:
-
-"""
 downpayment = total_cost*portion_down_payment
 
-x = 250000
+x = 150000
+
+tolerance = 100
 
 high = max(1.0, x)
 
 low = 0.0
 
+numGuesses = 0
+
 ans = (high + low)//2
 
-#Accepts downpayment within + or - 100
-while ans*36<=(downpayment - 100) or ans*36>=(downpayment+100):
-    print('low =', low, 'high =', high, 'ans =', ans)
-    numGuesses+=1
 
-    if ans*36 < downpayment:
+while abs(current_savings - downpayment) >= tolerance:
+    #print('low =', low, 'high =', high, 'ans =', ans)
+    current_savings = 0
+
+    # used in for loop
+    for_annual_salary = annual_salary
+
+    rate = ans/x
+
+    for month in range(36):
+
+        if month%6 == 0 and month > 0:
+            for_annual_salary += for_annual_salary*annual_raise
+            #print('annual raise applied:', annual_salary)
+        
+        monthly_salary = for_annual_salary/12
+
+        # Calc current_savings
+        current_savings += monthly_salary*rate + (current_savings*roi/12)
+
+
+
+    if current_savings < downpayment:
         low = ans
     else:
         high = ans
     
     ans = (high + low)//2
-
-print(numGuesses)
-
-
-while month_count != 36 and current_savings <= downpayment:
-    month_count+=1
-
-    if month_count%6 ==0:
-        annual_salary += annual_salary*annual_raise
-        #print('annual raise applied:', annual_salary)
-
-    savings_rate = ans/(annual_salary/12)
-    #print("{:.4f}".format(savings_rate))
-
-    
-    current_savings += savings_rate*(annual_salary/12)
-    print('current savings:',current_savings,'at month count:',month_count)
-
-    # ROI
-    current_savings += current_savings*r/12
-    
+    numGuesses+=1
+    if numGuesses>13:
+        break
 
 
-#print('current savings:',current_savings)
-#print('month count:',month_count)
-
+print('Best savings rate: {:.4f}'.format(rate))
+print('numGuesses in bisection search:', numGuesses)
